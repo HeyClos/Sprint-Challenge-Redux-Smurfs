c0ededa1fc56d63434bf0c84789d9d4c3226c1d8
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-05-31 10:17:13 -0700

    actions and reducers complete

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { FETCHING, CREATING, UPDATING, DELETING } from "../actions";
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -13,11 +13,62 @@
    error: null
  }
 */
+const initialState = {
+    smurfs: [],
+    fetchingSmurfs: false,
+    addingSmurf: false,
+    updatingSmurf: false,
+    deletingSmurf: false,
+    error: null
+  }
 
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
+  This will guard your namespacing issues. WHAT DOES THIS MEAN?!
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:   
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        addingSmurf: false,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: null
+      }
+      case CREATING:
+        return {
+          ...state,
+          fetchingSmurfs: false,
+          addingSmurf: true,
+          updatingSmurf: false,
+          deletingSmurf: false,
+          error: null
+      };
+      case UPDATING:
+        return {
+          ...state,
+          fetchingSmurfs: false,
+          addingSmurf: false,
+          updatingSmurf: true,
+          deletingSmurf: false,
+          error: null
+      };
+      case DELETING:
+        return {
+          ...state,
+          fetchingSmurfs: false,
+          addingSmurf: false,
+          updatingSmurf: false,
+          deletingSmurf: true,
+          error: null
+      };
+
+    default:
+      return state;
+  }
+};
